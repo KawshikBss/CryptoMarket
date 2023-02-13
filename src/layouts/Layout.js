e4ec1e92cb05c7f1e71dc34b6e75a908713b1dad
KawshikBss <kawshikbss@gmail.com>
2023-02-14 01:54:55 +0600

    added header

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+import "./layout.css";
 
 function Layout({ children }) {
-    return <div>{children}</div>;
+    return (
+        <div className="container">
+            <Sidebar />
+            <div className="body-section">
+                <Header />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
 }
 
 export default Layout;
