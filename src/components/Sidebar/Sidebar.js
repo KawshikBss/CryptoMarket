ba64cb4739fe971b47d63ff6bae708dd01e07d13
KawshikBss <kawshikbss@gmail.com>
2023-02-13 23:04:51 +0600

    created sidebar and style sheet

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import routes from '../../routes'
+import './sidebar.css'
+import {BsBorderAll, BsHeart, BsStar, BsGear} from 'react-icons/bs'
+import {BiUserCircle, BiExit} from 'react-icons/bi'
+
+const Sidebar = () => {
+  return (
+    <div className='wrapper'>
+        <ul className='nav-link-items'>
+                        <li className='nav-link-item nav-link-active'>
+                            <BsBorderAll className='nav-link-icon' />
+                        </li>
+                        <li className='nav-link-item'>
+                            <BsHeart className='nav-link-icon' />
+                        </li>
+                        <li className='nav-link-item'>
+                            <BsStar className='nav-link-icon' />
+                        </li>
+                        <li className='nav-link-item'>
+                            <BiUserCircle className='nav-link-icon' />
+                        </li>
+                        <li className='nav-link-item'>
+                            <BsGear className='nav-link-icon' />
+                        </li>
+        </ul>
+                        <span className='nav-link-item'>
+                            <BiExit className='nav-link-icon' />
+                        </span>
+    </div>
+  )
+}
+
+export default Sidebar
