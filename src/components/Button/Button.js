ad3662fd70daa413b59617f97c528c063929c43d
KawshikBss <kawshikbss@gmail.com>
2023-02-14 20:06:09 +0600

    modified full width

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./button.css";
 
-const Button = ({ children, color }) => {
+const Button = ({ children, color, full=false }) => {
     return (
-        <div className="button-container" style={{ backgroundColor: color }}>
+        <div className="button-container" style={{ backgroundColor: color, width: full? '100%': 'fit-content' }}>
             {children}
         </div>
     );
