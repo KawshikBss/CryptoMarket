b22f57e441f68e3b298c9825331d290e9ded107d
KawshikBss <kawshikbss@gmail.com>
2023-02-14 20:06:33 +0600

    completed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './card.css'
+import React from "react";
+import Button from "../Button/Button";
+import "./card.css";
 
-const Card = ({item}) => {
-  return (
-    <div>Card</div>
-  )
-}
+const Card = ({ item }) => {
+    return (
+        <div className="card-container">
+            <img src="/assets/Card.png" className="card-image" />
+            <div className="card-info">
+                <span className="card-title">Liquid Wave</span>
+                <span className="card-section">
+                    <span className="card-auc">Auction Time</span>
+                    <div className="card-bid-wrapper">
+                        <span className="card-bid">Current Bid</span>
+                        <span className="card-bid-price">0.05 ETH</span>
+                    </div>
+                </span>
+                <span className="card-section last">
+                    <span className="card-auc-time">3h 1m 50s</span>
+                    <span className="card-bid-price-final">0.15 ETH</span>
+                </span>
+            </div>
+            <Button full>Place A Bid</Button>
+        </div>
+    );
+};
 
-export default Card
+export default Card;
