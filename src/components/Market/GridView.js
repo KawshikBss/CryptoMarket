b58b49fac226ba2ecbd567892560d3a4cf6d3c6b
KawshikBss <kawshikbss@gmail.com>
2023-02-16 23:59:24 +0600

    fixed unique key error

--- src/components/Market/GridView.js
+++ src/components/Market/GridView.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./grid.css";
 import Card from "../Card/Card";
 
-const GridView = ({ items = [0,1,2,3,4] }) => {
+const GridView = ({ items = [0, 1, 2, 3, 4] }) => {
     return (
         <div className="grid-items">
             {items
                 ? items.map((item, index) => {
-                      return <Card />;
+                      return <Card key={index} />;
                   })
                 : ""}
         </div>
