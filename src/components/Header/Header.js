bc85fb2aa0d69fada10cbbd4c95dfa2233c92031
KawshikBss <kawshikbss@gmail.com>
2023-02-17 00:00:05 +0600

    added useWallet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import './header.css'
-import {BsSearch, BsBell} from 'react-icons/bs'
-import {FaUserCircle} from 'react-icons/fa'
+import React from "react";
+import "./header.css";
+import { BsSearch, BsBell } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+import useWallet from "../../hooks/useWallet";
 
 const Header = () => {
-  return (
-    <div className='header-container'>
-        <span className='search-wrapper'>
-            <BsSearch className='search-btn' />
-            <input type='text' placeholder='Search Here' className='search-input' />
-        </span>
-        <span className='user-section'>
-            <BsBell className='header-btn' />
-            <FaUserCircle className='header-btn' />
-        </span>
-    </div>
-  )
-}
+    const { account, balance, connectWallet } = useWallet();
+    return (
+        <div className="header-container">
+            <span className="search-wrapper">
+                <BsSearch className="search-btn" />
+                <input
+                    type="text"
+                    placeholder="Search Here"
+                    className="search-input"
+                />
+            </span>
+            <span className="user-section">
+                <BsBell className="header-btn" />
+                <FaUserCircle className="header-btn" onClick={connectWallet} />
+                {account ? <span>{balance ? balance : "0"} ETH</span> : ""}
+            </span>
+        </div>
+    );
+};
 
-export default Header
+export default Header;
