be2695e921b6d730cc662cbcf23b092c008e3efb
KawshikBss <kawshikbss@gmail.com>
2023-02-17 00:00:43 +0600

    fixed filter buttons

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import './market.css'
-import GridView from './GridView'
+import React, { useState } from "react";
+import "./market.css";
+import GridView from "./GridView";
+import useContract from "../../hooks/useContract";
 
-const Market = ({items}) => {
-  return (
-    <div className='market-container'>
-        <div className='market-header'>
-            <h4 className='section-heading'>Trending Bids</h4>
-            <span className='section-filters'>
-                <span className='filter'>All</span>
-                <span className='filter'>Artwork</span>
-                <span className='filter'>Book</span>
-            </span>
+const Market = ({ items }) => {
+    const { nftList } = useContract();
+    const [filter, setFilter] = useState("All");
+    const changeFilter = (filterName) => {
+        setFilter((curr) => {
+            return curr === filterName ? "All" : filterName;
+        });
+    };
+    const isFilterActive = (filterName) => {
+        return filter === filterName;
+    };
+    return (
+        <div className="market-container">
+            <div className="market-header">
+                <h4 className="section-heading">Trending Bids</h4>
+                <span className="section-filters">
+                    <span
+                        className={`filter ${
+                            isFilterActive("All") ? "active" : ""
+                        }`}
+                        onClick={() => changeFilter("All")}
+                    >
+                        All
+                    </span>
+                    <span
+                        className={`filter ${
+                            isFilterActive("Artwork") ? "active" : ""
+                        }`}
+                        onClick={() => changeFilter("Artwork")}
+                    >
+                        Artwork
+                    </span>
+                    <span
+                        className={`filter ${
+                            isFilterActive("Book") ? "active" : ""
+                        }`}
+                        onClick={() => changeFilter("Book")}
+                    >
+                        Book
+                    </span>
+                </span>
+            </div>
+            <GridView />
         </div>
-        <GridView />
-    </div>
-  )
-}
+    );
+};
 
-export default Market
+export default Market;
